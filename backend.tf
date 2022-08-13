946181f33f90cc964d1e7add7666e38ec3aac396
hb-netops <111117035+hb-netops@users.noreply.github.com>
2022-08-13 13:02:16 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "hbcorp"
+    organization = "hbcorp"
     workspaces {
       name = "ace-iac-day-zero"
     }
